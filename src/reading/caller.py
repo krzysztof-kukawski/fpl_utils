9a57d163cb8a676fea400d10b079649327147ede
krzysztof-kukawski <krzysztof.kukawski.ln@gmail.com>
2025-02-02 02:20:09 +0100

    pylint fix

--- src/reading/caller.py
+++ src/reading/caller.py
@@ -1,3 +1,7 @@
+"""
+FPL api caller
+"""
+
 import json
 
 import requests
@@ -7,21 +11,40 @@ from src.reading.players import PlayerRoster
 
 
 class FPLCaller:
+    """
+    FPL api caller class, gets names of players from a single url,
+    then gets their per game week stats
+    """
     def __init__(self, roster_class: type[PlayerRoster]) -> None:
         self._roster_class = roster_class
         self.roster: PlayerRoster
 
     def get_names_from_api(self, url: str) -> None:
+        """
+
+        :param url:
+        :return:
+        """
         data = self._get(url)
 
         self.roster = self._roster_class(data["elements"])
 
     def get_gw_history_from_api(self, url: str, gw: int) -> None:
+        """
+
+        :param url:
+        :param gw:
+        :return:
+        """
         data = self._get(url)
 
         self.roster.extend_history(gw, data["elements"])
 
     def get_roster(self) -> PlayerRoster:
+        """
+
+        :return:
+        """
         return self.roster
 
     @staticmethod
